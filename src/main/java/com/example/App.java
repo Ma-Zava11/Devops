940d0dc5c0195dfd473cb6677f8be9e6b7414303
Ma-Zava11 <mazavasoatianajennylaurent@gmail.com>
2025-07-23 23:55:41 +0200

    Initial commit for DevOps project

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps pipeline!");
+        System.out.println("Hello DevOps");
     }
 }
